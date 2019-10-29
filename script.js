88ff36e4a60c67268b23f4de509a4bbd3c227192
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2019-10-29 14:42:20 +0100

    it just works

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-setInterval(function() {
-    console.log("bread")
-}, 1000)
+let bgArr = document.querySelectorAll(".image-slide > div")
+
+//setInterval(flipThrough, 3000)
+
+let i = 0
+function flipThrough() {
+    bgArr[0].style.opacity = "1"
+    bgArr[1].style.opacity = "0"
+    bgArr[2].style.opacity = "0"
+    bgArr[3].style.opacity = "0"
+    if(i === 0){
+        bgArr[i].style.opacity = "1"
+        bgArr[i].style.transition = "1s ease"
+        i++
+    }else {
+        bgArr[i].style.opacity = "1"
+        bgArr[i-1].style.opacity = "0"
+        bgArr[i].style.transition = "1s ease"
+        i++
+        if (i === 4) {
+            i = 0
+        }
+    }
+}
+flipThrough()
+
+
